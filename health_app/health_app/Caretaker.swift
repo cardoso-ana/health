e6b7bc57fced9b0897d7da521831a65e455afe90
Pedro de Sá <pedro.desa@live.com>
2016-07-14 15:21:25 -0300

    Criação das classes
    pedroca fodão

--- health_app/health_app/Caretaker.swift
+++ health_app/health_app/Caretaker.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+public class Caretaker {
+    
+    static let singleton = Caretaker()
+    
+    var id = ""
+    var elderName = ""
+    var elderAge = 0
+    var elderPhone = ""
+    
+    
+    private init() {
+        
+    }
+    
+    func getElderName() -> String {
+        return elderName
+    }
+    
+    func setElderName(name: String) {
+        self.elderName = name
+    }
+    
+    
+    func getId() -> String {
+        return id
+    }
+    
+    func setId(id: String) {
+        self.id = id
+    }
+    
+    
+    func getElderAge() -> Int {
+        return elderAge
+    }
+    
+    func setElderAge(age: Int) {
+        self.elderAge = age
+    }
+    
+    
+    func getElderPhone() -> String {
+        return elderPhone
+    }
+    
+    func setElderPhone(phone: String) {
+        self.elderPhone = phone
+    }
+    
+    
+}
--- health_app/health_app/Elder.swift
+++ health_app/health_app/Elder.swift
@@ -7,3 +7,93 @@
 //
 
 import Foundation
+
+public class Elder {
+    
+    static let singleton = Elder()
+    
+    var name = ""
+    var age = 0
+    var phone = ""
+    var street = ""
+    var houseNumber = ""
+    var city = ""
+    var state = ""
+    var careTakerId = ""
+    
+    private init() {
+        
+    }
+    
+    
+    func getElderName() -> String {
+        return name
+    }
+    
+    func setElderName(name: String) {
+        self.name = name
+    }
+    
+    
+    func getElderAge() -> Int {
+        return age
+    }
+    
+    func setElderAge(age: Int) {
+        self.age = age
+    }
+    
+    
+    func getElderPhone() -> String {
+        return phone
+    }
+    
+    func setElderPhone(phone: String) {
+        self.phone = phone
+    }
+    
+    
+    func getElderStreet() -> String {
+        return street
+    }
+    
+    func setElderStreet(street: String) {
+        self.street = street
+    }
+    
+    
+    func getElderHouseNumber() -> String {
+        return houseNumber
+    }
+    
+    func setElderHouseNumber(number: String) {
+        self.houseNumber = number
+    }
+    
+    
+    func getElderCity() -> String {
+        return city
+    }
+    
+    func setElderCity(city: String) {
+        self.city = city
+    }
+    
+    
+    func getElderState() -> String {
+        return state
+    }
+    
+    func setElderState(state: String) {
+        self.state = state
+    }
+    
+    
+    func getEldercareTakerId() -> String {
+        return careTakerId
+    }
+    
+    func setEldercareTakerId(id: String) {
+        self.careTakerId = id
+    }
+}
