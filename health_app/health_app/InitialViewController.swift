fca3f409ebbf8a1d9980f3636020961ddf2583a1
cardoso-ana <AnaCardoso@Anas-MacBook-Pro-2.local>
2016-07-19 13:43:46 -0300

    Telas do idoso
    PEDRO AJEITA OS BOTÃO e faz o cloud da tela de cadastro

--- health_app/health_app/InitialViewController.swift
+++ health_app/health_app/InitialViewController.swift
@@ -17,11 +17,7 @@ class InitialViewController: UIViewController
         super.viewDidLoad()
     }
     
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
+
+
