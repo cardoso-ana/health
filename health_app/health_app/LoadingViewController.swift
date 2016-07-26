8076d8c5c515c0f3c95735dcffd70ad0a871bcb3
cardoso-ana <AnaCardoso@Anas-MacBook-Pro-2.local>
2016-07-26 11:27:54 -0300

    tela de loading

---
+++ health_app/health_app/LoadingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingViewController.swift
+//  health_app
+//
+//  Created by Ana Carolina Nascimento on 7/26/16.
+//  Copyright © 2016 Ana Carolina Nascimento. All rights reserved.
+//
+
+import UIKit
+
+
+class LoadingViewController: UIViewController
+{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
--- health_app/health_app/MonitorViewController.swift
+++ health_app/health_app/MonitorViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 import MapKit
 import WatchConnectivity
 
-class MonitorViewController: UIViewController, WCSessionDelegate {
+struct AdressMonitor
+{
+    var coordinate : CLLocationCoordinate2D?
+    var thoroughfare : String?
+    var subThoroughfare : String?
+    var subLocality : String?
+    var locality : String?
+    var administrativeArea : String?
+    var country : String?
+}
+
+class MonitorViewController: UIViewController, WCSessionDelegate, MKMapViewDelegate
+{
     
     @IBOutlet weak var gradientView: UIView!
     
@@ -23,6 +35,7 @@ class MonitorViewController: UIViewController, WCSessionDelegate {
     @IBOutlet weak var monitorImage: UIImageView!
     @IBOutlet weak var monitorLabel: UILabel!
     
+    var adress = AdressMonitor()
     
     var idCt = ""
     let progressHUD = ProgressHUD(text: "Carregando")
@@ -30,21 +43,25 @@ class MonitorViewController: UIViewController, WCSessionDelegate {
     var latIodoso = ""
     var longIdoso = ""
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        map.delegate = self
+        map.showsUserLocation = true
         
         self.view.addSubview(progressHUD)
         progressHUD.show()
         
         CloudKitDAO().pegaIdoso(id: self.idCt)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(MonitorViewController.malandramente), name: "véioChegando" as NSNotification.Name, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MonitorViewController.setMapToUserLocation), name: "véioChegando" as NSNotification.Name, object: nil)
         
         
     }
     
     
-    func malandramente(notification: NSNotification) {
+    func setMapToUserLocation(notification: NSNotification) {
         let dict = notification.object as! NSDictionary
         
         print("PRINTAAANDO OIEEEE")
@@ -57,10 +74,86 @@ class MonitorViewController: UIViewController, WCSessionDelegate {
         
         print("PRINTAAANDO LAT E LONG: \(latIodoso) e \(longIdoso)")
         
+        let center = CLLocationCoordinate2D(latitude: CLLocationDegrees(latIodoso)!, longitude: CLLocationDegrees(longIdoso)!)
+        let location = CLLocation(latitude: CLLocationDegrees(latIodoso)!, longitude: CLLocationDegrees(longIdoso)!)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.026, longitudeDelta: 0.026))
+        self.map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        
+        
+        CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error)-> Void in
+            if (error != nil)
+            {
+                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
+                return
+            }
+            
+            if placemarks!.count > 0
+            {
+                let pm = (placemarks?[0])! as CLPlacemark
+                print("\n\n\n************aqui******************\n\n\n")
+                print([String(pm.location?.coordinate), String(pm.thoroughfare), String(pm.subThoroughfare), String(pm.subLocality), String(pm.locality), String(pm.administrativeArea)])
+                
+                self.adress.coordinate = pm.location?.coordinate
+                self.adress.thoroughfare = String((pm.thoroughfare)) ?? ""
+                self.adress.subThoroughfare = String((pm.subThoroughfare)) ?? ""
+                self.adress.subLocality = String((pm.subLocality)) ?? ""
+                self.adress.locality = String((pm.locality)) ?? ""
+                self.adress.administrativeArea = String((pm.administrativeArea)) ?? ""
+                self.adress.country = String((pm.country)) ?? ""
+                
+                
+                
+                print(self.adress)
+                
+                if self.adress.thoroughfare != nil && self.adress.subThoroughfare != nil && self.adress.administrativeArea != nil && self.adress.locality != nil && self.adress.country != nil
+                {
+                    annotation.title = ((self.adress.thoroughfare!) + ", " + (self.adress.subThoroughfare!))
+                    annotation.subtitle = ((self.adress.locality!) + ", " + (self.adress.administrativeArea!) + " - " + (self.adress.country)!)
+                    
+                    annotation.title = annotation.title?.replacingOccurrences(of: "Optional", with: "")
+                    annotation.title = annotation.title?.replacingOccurrences(of: "(\"", with: "")
+                    annotation.title = annotation.title?.replacingOccurrences(of: "\")", with: "")
+                    
+                    annotation.subtitle = annotation.subtitle?.replacingOccurrences(of: "Optional", with: "")
+                    annotation.subtitle = annotation.subtitle?.replacingOccurrences(of: "(\"", with: "")
+                    annotation.subtitle = annotation.subtitle?.replacingOccurrences(of: "\")", with: "")
+                }
+                
+            }
+            else
+            {
+                print("Problem with the data received from geocoder")
+            }
+        })
+        
+        self.map.removeAnnotations(self.map.annotations)
+        self.map.addAnnotation(annotation)
+
+        
         progressHUD.hide()
     }
     
-    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        let userIdentifier = "UserLocation"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: userIdentifier)
+        if annotationView == nil
+        {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: userIdentifier)
+            annotationView!.canShowCallout = true
+            annotationView!.image = UIImage(named: "location")
+        }
+        else
+        {
+            annotationView!.annotation = annotation
+        }
+        
+        return annotationView
+    }
     
     
     
